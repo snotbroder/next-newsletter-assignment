0821a612b85cbdea5957f470ea7c63014b777b84
Joris <jote0002@stud.kea.dk>
2024-11-29 08:54:12 +0100

    hmm

--- src/app/update/[id]/page.jsx
+++ src/app/update/[id]/page.jsx
@@ -1,12 +1,9 @@
+async function page({ params }) {
+  const { id } = await params;
+  const subscriber = await getSubById(id);
+  async function updateAction(params) {}
 
-async function page({params}) {
- const { id } = await params;
- const subscriber = await getSubById(id);
-async function updateAction(params) {
-  
-}
-
-    /*TODO:
+  /*TODO:
     - Hent subscriber med id fra params
     - Lav en formular med input for name og email med "defaultValue" sat til subscriberens nuværende værdier
     - Lav en funktion med "use server" som køres fra en action på formen
@@ -15,15 +12,9 @@ async function updateAction(params) {
     importer din PATCH-funktion fra supabase og kør den med objektet som argument
     - Brug revalidatePath("/") til at genindlæse siden, hvor du viser alle subscribers
     - Brug redirect("/") til at sende brugeren tilbage til forsiden
-    */ 
+    */
 
-
-
-  return (
-    <form action={}>
-
-    </form>
-  )
+  return <form></form>;
 }
 
-export default page
+export default page;
